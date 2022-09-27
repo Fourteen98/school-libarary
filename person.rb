e37ee606adb48a6d5faa7d5b866fff61c7ba0ca5
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-27 18:05:15 +0000

    fix reviewers comment

--- person.rb
+++ person.rb
@@ -1,11 +1,16 @@
 class Person
-  attr_accessor :id, :name, :age
+  attr_accessor :name, :age
+  attr_reader :id
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize( age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
@@ -14,7 +19,4 @@ class Person
     @age >= 18
   end
 
-  def can_use_services?
-    of_age? or @parent_permission
-  end
 end
