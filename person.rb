304d8d9f3c31bde114136a540e8bd43d1cc40160
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-27 18:10:50 +0000

    fix linters

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize( age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -18,5 +18,4 @@ class Person
   def of_age?
     @age >= 18
   end
-
 end
