3985e11029bfedfa6c66fdf1e70ff76c8247cfe3
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-05 19:54:35 +0000

    add test for person

--- spec/Test_Person_spec.rb
+++ spec/Test_Person_spec.rb
@@ -4,7 +4,10 @@ describe Person do
   context "When creating a Person" do
 
     it "Should create a new Person" do
-      person = Person.new(age: "34", name: "Idrees")
+      person = Person.new(34, 'Idrees')
+
+      expect(person.name).to eq "Idrees"
+      expect(person.age).to eq 34
     end
   end
 end
