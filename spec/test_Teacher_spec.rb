6b7ea9190f1ac97c48f5ad6bdd005dcc02be43d0
jaferIdrees <jaferll@gmail.com>
2022-10-05 23:03:15 +0300

    Add testing for Teacher class

---
+++ spec/test_Teacher_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../classes/teacher'
+
+describe Teacher do
+  context "When creating a Teacher" do
+
+    teacher = Teacher.new('Math', 22,'Elias')
+    it "Should return 'Elias' as a teacher name" do
+      puts teacher.name
+      expect(teacher.name).to eq "Elias"
+    end
+
+    it "Should return '22' as a teacher age" do
+      puts teacher.name
+      expect(teacher.age).to eq 22
+    end 
+    
+    it "Should return 'Math' as a teacher specialization" do
+      puts teacher.name
+      expect(teacher.specialization).to eq 'Math'
+    end 
+  end
+end
